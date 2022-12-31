4be5bf0f42dbbe0d385594a7a7cbc1ea1fa599d8
Roy <ekeneroy@gmail.com>
2022-12-31 09:01:33 -0600

    loops and which school

---
+++ loops.js
@@ -0,0 +1,30 @@
+// Let us take an iteration variable, that represent the remaining Time in seconds
+n = 60
+
+// While loop with a stop condition
+while (n >= 0) {
+    if (n === 50) {
+        console.log("Orbiter transfers from ground to internal power");
+    }
+    else if (n === 31) {
+        console.log("Ground launch sequencer is go for auto sequence start");
+    }
+    else if (n === 16) {
+        console.log("Activate launch pad sound suppression system");
+    }
+    else if (n === 10) {
+        console.log("Activate main engine hydrogen burnoff system");
+    }
+    else if (n === 6) {
+        console.log("Main engine start");
+    }
+    else if (n === 0) {
+        console.log("Solid rocket booster ignition and liftoff!");
+    }
+    else{
+        console.log("T-"+n+" seconds");
+    }
+
+    //Never forget to decrement/increment the iteration variable in a while loop
+    // Otherwise, you loop will run infinite iterations
+    n = n-1;
---
+++ whichSchool.js
@@ -0,0 +1,5 @@
+const whichSchool  = function (age) { 
+  if (age >= 1 && age <= 12 ) { return ( "Elementary School") }
+  else if (age >= 13 && age <= 18){ return ( "Secondary School") }
+  else if (age > 18){ return ( "Lighthouse Labs") }
+}
